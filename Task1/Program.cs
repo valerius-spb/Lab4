5678d679a053f7b455da74cbf30f68050a41f79e
Valeriy Dubovov <valerius.spb@yandex.ru.ru>
2022-11-04 02:32:24 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int kv = 0;
+            Console.WriteLine("Квадраты чисел от 1 до {0}:", n);
+            //for (int i = 1; i <= 2*n-1; i+=2)
+            //цикл выведения квадрата "n"чисел 
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}^2 = {1}", i, kv += i * 2 - 1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность целых чисел, для окончания ввода последовательности введите 0:");
+
+            int a = 0, k1 = 0, k2 = 0;
+
+            do
+            {
+                a = Convert.ToInt32(Console.ReadLine());//Ввод чисел последовательности
+                if (a > 0)
+                {
+                    k1++; //Количество положительных чисел
+                   
+                }
+                else if (a < 0)
+                {
+                    k2++; //Количество отрицательных чисел
+                    
+                }
+            } while (a != 0);
+            Console.WriteLine("Количество положительных чисел = " + k1);
+            Console.WriteLine("Количество отрицательных чисел = " + k2);
+            Console.ReadKey();
+        }
+    }
+}
